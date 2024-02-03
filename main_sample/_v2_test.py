487808aa28887fb369f9e61de238ce72fc5d2f04
olma <143671249+om0427@users.noreply.github.com>
2024-02-03 17:21:14 +0900

    一般化

--- main_sample/_v2_test.py
+++ main_sample/_v2_test.py
@@ -2,31 +2,28 @@ import openpyxl
 from qgis.core import QgsVectorLayer, QgsFeature, QgsPointXY, QgsGeometry, QgsProject
 
 # Excelファイルを読み取ります
-excel_file_path = r'C:\仕事\課題\R5_I4\地域学\outoveexcel.xlsx'
+excel_file_path = r'Excelファイルのパス' //中身を自分のパスに書き換えてね
 wb = openpyxl.load_workbook(excel_file_path)
 sheet = wb.active
 
 # QGISプロジェクトをロードします
 project = QgsProject.instance()
-project.read(r'C:\仕事\課題\R5_I4\地域学\outo_v2.qgs')
+project.read(r'QGISのプロジェクトファイルのパス')//中身を自分のパスに書き換えてね
 
 # マップレイヤーを取得します
-layer = project.mapLayersByName('outoveexcel-data1')[0]  # レイヤー名を適切に変更してください
+layer = project.mapLayersByName('追加したレイヤー名')[0]  # レイヤー名を適切に変更してください
 
 # Excelからデータを読み取り、プロットします
 for row in sheet.iter_rows(min_row=2, values_only=True):
-    x, y = row[1], row[2]  # Excelの列に応じて調整
-    
-    # xとyがNoneでないことを確認
-    if x is not None and y is not None:
-        point = QgsPointXY(x, y)
-        
-        # QgsPointXY オブジェクトを QgsGeometry オブジェクトに変換
-        geometry = QgsGeometry.fromPointXY(point)
-        
-        feature = QgsFeature()
-        feature.setGeometry(geometry)
-        layer.dataProvider().addFeatures([feature])
+x, y = row[1], row[2]  # Excelの列に応じて調整
+point = QgsPointXY(x, y)
+
+# QgsPointXY オブジェクトを QgsGeometry オブジェクトに変換
+geometry = QgsGeometry.fromPointXY(point)
+
+feature = QgsFeature()
+feature.setGeometry(geometry)
+layer.dataProvider().addFeatures([feature])
 
 # マップを更新します
 layer.triggerRepaint()
